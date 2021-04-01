b11e530f04a1e16292912207a04aec7b59c31709
pavel <pavel8897@bk.ru>
2021-04-01 22:54:56 +0300

    second

--- src/App.js
+++ src/App.js
@@ -5,32 +5,33 @@ import './index.css';
 
 const App = () => {
 	const [quest, setQuest] = useState([
-		{quest: 'Где производят автомобили Bugatti?', answer: 'Франция', option: '', flag: false},
-		{quest: 'Какая компания производит наибольшее количество шин в мире', answer: 'Лего', option: '', flag: false},
-		{quest: 'Где находится самая большая пустыня?', answer: 'Антарктида', option: '', flag: false}
+		{quest: 'Где производят автомобили Bugatti?', answer: 'Франция', option: '', flag: ''},
+		{quest: 'Какая компания производит наибольшее количество шин в мире', answer: 'Лего', option: '', flag: ''},
+		{quest: 'Где находится самая большая пустыня?', answer: 'Антарктида', option: '', flag: ''}
 	])	
 
 	const setOption = (e, index) => {
-		quest[index].option = e.target.value
-		setQuest(quest)
+		quest[index].option = (e.target.value).trim()
+		setQuest(quest)		
 	}
 
 	const getOption = (index) => {
-		/*let option = document.querySelector('#option'+index)*/
-		quest[index].option == quest[index].answer ? quest[index].flag = true : quest[index].flag = false
-		setQuest(quest)
+		const newArr = quest.slice()
+		newArr[index].option == newArr[index].answer ? newArr[index].flag = true : newArr[index].flag = false
+		setQuest(newArr)
 	}
-
+	
 	const work = quest.map((item, index)=>{
 		return <Question 
 			quest = {item.quest}
 			answer = {item.answer}
 			option = {item.option}
 			key = {index}
-			index = {index}	
-			getOption = {getOption}
+			index = {index}			
 			setOption = {setOption}
+			getOption = {getOption}
 			flag = {item.flag}
+			item = {item}
 		/>
 	})
 	
--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './index.css';
 
-const Question = (props) => {
-	return (		
+const Question = (props) => {	
+	return (
 		<div>
 			<p>{props.quest}</p>
-			{props.flag ?
-			<p>Your answer</p>			
+			<input id={'option' + props.index} onChange={(e) => props.setOption(e, props.index)} defaultValue={props.option} />
+			{(typeof props.flag == "boolean") ?			
+				props.flag ?
+				<p className="green">Ваш ответ {props.option} правильнй</p>			
+				:
+				<div>
+					<p className="red">Ваш ответ {props.option} не правильный</p>
+					<p className="green">Правильный ответ {props.answer}</p>
+				</div>
 			:
-			<input id={'option' + props.index} onChange={(e) => props.setOption(e, props.index)} defaultValue={props.option} />	
+			<p></p>
 			}
 			<input type="submit" value="Сдать тест" onClick={() => props.getOption(props.index)}  />
 		</div>
